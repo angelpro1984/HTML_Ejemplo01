c3bde0e4cb856aaa6e1b673a6377a68ea321e9a6
angelpro1984 <angelpro1984@gmail.com>
2024-01-23 03:04:46 -0500

    Inicial

---
+++ Ejemplo.php
@@ -0,0 +1,9 @@
+<head>
+<title>Creación de un portal con PHP y MySQL</title>
+</head>
+<?php
+define ("capitalespana" , "Madrid"); /* capitalespana tendrá durante la
+ejecución de la página siempre el valor Madrid. */
+define ("habitantes" , 4000); /* habitantes tendrá durante la ejecución
+de la página siempre el valor 4.000.000. */
+?>
